93e1217c0ca6f46f988e9de9e75a3b0fbf89d026
bharisagar <74423237+bharisagar@users.noreply.github.com>
2026-06-11 19:28:27 +0530

    Delete sql directory

--- sql/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE students (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    course VARCHAR(100)
-);
-
-INSERT INTO students (name, course) VALUES ('Chetan', 'Computer Science');
-INSERT INTO students (name, course) VALUES ('Manjunath', 'Mathematics');
-INSERT INTO students (name, course) VALUES ('Suresh', 'Physics');
